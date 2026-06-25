2aeb7af44be377587a18a2439bfbe77729018ff3
Pedro Henrique De Lima Pinheiro <00001149065953sp@aluno.educacao.sp.gov.br>
2026-06-25 13:20:30 -0300

    atualização

--- exercicio16.py
+++ exercicio16.py
@@ -1,5 +1,5 @@
 n1=float(input("Digite o primeiro numero: "))
-n2=float(input("Digite o segudo numero: "))
+n2=float(input("Digite o segundo numero: "))
 if n1>n2:
     print(f"O maior numero é: {n1}")
 elif n2>n1:
